791c0da4efccbd7d742e02deca602e99c12d4926
Kelvin Lima <kelvader18@gmail.com>
2023-08-25 00:38:14 -0300

    logica que remove mensagem apos fechamento do pr

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+	"github.com/redis/go-redis/v9"
 )
+
+var redisClient *redis.Client
 
 type GitHubPullRequest struct {
 	Action     string `json:"action"`
@@ -18,6 +23,7 @@ type GitHubPullRequest struct {
 		Name string `json:"name"`
 	} `json:"repository"`
 	PullRequest struct {
+		Id   int `json:"id"`
 		User struct {
 			Login string `json:"login"`
 		} `json:"user"`
@@ -26,11 +32,25 @@ type GitHubPullRequest struct {
 	} `json:"pull_request"`
 }
 
-var discord = connectToDiscord()
+var discord *discordgo.Session
 
 func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file")
+	}
+
+	discord = connectToDiscord()
+
+	redisClient = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: os.Getenv("REDIS_PASS"),
+		DB:       0,
+	})
+
+	ctx := context.Background()
+	if _, err := redisClient.Ping(ctx).Result(); err != nil {
+		log.Println("Error connecting to Redis: ", err)
+		return
 	}
 
 	log.Println(discord.State.User.Username + " is now running.")
@@ -54,18 +74,29 @@ func gitHubWebHookHandler(w http.ResponseWriter, r *http.Request) {
 
 	if pr.Action == "opened" {
 		message := fmt.Sprintf("%s: %s", pr.Repository.Name, pr.PullRequest.HTMLURL)
-		sendDiscordMessage(pr, message)
+		value := sendDiscordMessage(pr, message)
+
+		redisKey := strconv.Itoa(pr.PullRequest.Id)
+		redisValue := value.ID
+
+		redisClient.Set(r.Context(), redisKey, redisValue, 0)
+	} else if pr.Action == "closed" {
+		discordMessageId := redisClient.Get(r.Context(), strconv.Itoa(pr.PullRequest.Id)).Val()
+		discord.ChannelMessageDelete(os.Getenv("DISCORD_CHANNEL_ID"), discordMessageId)
+		redisClient.Del(r.Context(), strconv.Itoa(pr.PullRequest.Id))
 	}
 }
 
-func sendDiscordMessage(pr GitHubPullRequest, message string) {
+func sendDiscordMessage(pr GitHubPullRequest, message string) *discordgo.Message {
 	discordChannelId := os.Getenv("DISCORD_CHANNEL_ID")
 
-	_, err := discord.ChannelMessageSend(discordChannelId, message)
+	value, err := discord.ChannelMessageSend(discordChannelId, message)
 
 	if err != nil {
 		log.Println("Error sending message to Discord: ", err)
 	}
+
+	return value
 }
 
 func connectToDiscord() *discordgo.Session {
